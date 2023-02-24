85828bb7791b1afd35b62a94d39681f89493e1c4
ricesush <pixelartme@gmail.com>
2023-02-25 06:33:24 +0800

    +details sidebar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Leftbar() {
     return (
         <React.Fragment>
-            <div className='rightbar p-2'>
+            <div className='rightbar p-2 shadow-lg'>
 
                 <div className='sticky-top'>
                     hello
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,54 +6,86 @@ import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/
 export default function Sidebar() {
     return (
         <React.Fragment>
-            <section className="col-lg-2 section-left my-auto ">
-                <div className="text-light text-center bg-gray py-4 shadow-lg">
+            <section className="col-lg-2 section-left my-auto shadow-lg">
+                <div className="text-light text-center bg-gray py-4 shadow">
                     <img src={myImage} width={100}></img>
                     <div className="pt-3 fw-bold ">
-                        E-J Santosidad
+                        Erick-John Santosidad
                     </div>
                     <div className="pb-3">
                         <small className="text-secondary">Full-Stack Web Developer</small>
                     </div>
-                    <div className="d-flex justify-content-around col-4 mx-auto align-items-center pt-2">
-                        <a href="https://www.facebook.com/RiceSush" className="text-light fs-5"><FontAwesomeIcon icon={brands('facebook')} /></a>
+                    <div className="text-center">
+                    <div className="d-flex justify-content-around col-4 mx-auto  align-items-center">
+                        <a href="https://www.facebook.com/RiceSush" className="text-light fs-5 "><FontAwesomeIcon icon={brands('facebook')} /></a>
                         <a href="https://github.com/ricesush" className="text-light fs-5"><FontAwesomeIcon icon={brands('github')} /></a>
                         <a href="https://www.linkedin.com/in/ejsan/" className="text-light fs-5"><FontAwesomeIcon icon={brands('linkedin')} /></a>
                     </div>
                 </div>
+                </div>
                 <div className="text-light px-5 pt-3">
                     <table class="table table-borderless text-light">
-                        <tbody className="">
+                        <tbody className=" ">
                             <tr>
-                                <td className="yellowText"><FontAwesomeIcon icon={icon({name: 'location-dot', style: 'solid'})} /></td>
+                                <td className="yellowText"><FontAwesomeIcon icon={icon({ name: 'location-dot', style: 'solid' })} /></td>
                                 <td>Quezon City, Philippines</td>
                             </tr>
                             <tr>
-                                <td className="yellowText"><FontAwesomeIcon icon={icon({name: 'envelope', style: 'solid'})} /></td>
+                                <td className="yellowText"><FontAwesomeIcon icon={icon({ name: 'envelope', style: 'solid' })} /></td>
                                 <td>Pixelartme@gmail.com</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
+
                 <hr className="text-light mx-3" />
-                <div className="text-light pt-3 p-3 pb-0">
-                    <small className="yellowText fw-bold">
-                        Tech Stack: 
-                    </small>
-                    <div className="fs-1 d-flex justify-content-around p-4 pt-4">
+                <div className="text-light pt-2 p-3 pb-0">
+                    <div className="text-center">
+                        <small className="yellowText  fw-bold">
+                            Front-End
+                        </small>
+                    </div>
+                    <div className="fs-1 d-flex justify-content-around p-4 pt-2">
                         <FontAwesomeIcon icon={brands('html5')} title="HTML5" />
                         <FontAwesomeIcon icon={brands('css3')} title="CSS3" />
-                        <FontAwesomeIcon icon={brands('js')} />
+                        <FontAwesomeIcon icon={brands('bootstrap')} />
+
                     </div>
                     <div className="fs-1 d-flex justify-content-around p-4 pt-0">
-                        <FontAwesomeIcon icon={brands('php')} />
+                        <FontAwesomeIcon icon={brands('js')} />
                         <FontAwesomeIcon icon={brands('react')} />
+                        <FontAwesomeIcon icon={brands('figma')} />
+                    </div>
+                    <div className="mt-2 text-center">
+                        <small className="yellowText fw-bold ">
+                            Back-End & Framework
+                        </small>
+                    </div>
+                    <div className="fs-1 d-flex justify-content-around p-4 pt-2">
+                        <FontAwesomeIcon icon={brands('php')} />
                         <FontAwesomeIcon icon={brands('laravel')} />
                     </div>
                 </div>
                 <hr className="text-light mx-3" />
+                <div className="text-light pt-2 p-3 pb-0">
+                <div className="text-center">
+                        <small className="yellowText  fw-bold">
+                            Work Preference
+                        </small>
+                    </div>
+                    <div className="fs-1 p-4 pt-2">
+                        <div className="fs-6">
+                        <FontAwesomeIcon icon={icon({ name: 'check', style: 'solid' })} className="yellowText pe-2"/> Full-time <br />
+                        <FontAwesomeIcon icon={icon({ name: 'check', style: 'solid' })} className="yellowText pe-2"/> Part-time<br />
+                        <FontAwesomeIcon icon={icon({ name: 'check', style: 'solid' })} className="yellowText pe-2"/> Freelance<br />
+                        <FontAwesomeIcon icon={icon({ name: 'check', style: 'solid' })} className="yellowText pe-2"/> Hybrid<br />
+                        <FontAwesomeIcon icon={icon({ name: 'check', style: 'solid' })} className="yellowText pe-2"/> Work from Home
+                        </div>
+                        
+                    </div>
+                </div>
             </section>
-
+            
 
         </React.Fragment>
     )
