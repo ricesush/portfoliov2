2e8c266f1a74ea091de85261f5772f7c13adbfcb
ricesush <pixelartme@gmail.com>
2023-02-25 09:21:36 +0800

    fixed text animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,6 @@
 import React from "react";
 
 export default function Home() {
-    const text = document.getElementById('sec-text');
-    const textLoad = () => {
-        setTimeout(() => {
-            text.innerText = "Web Developer2"
-        }, 0);
-
-        setTimeout(() => {
-            text.innerText = "Freelancer"
-        }, 4000);
-
-        setTimeout(() => {
-            text.innerText = "Passionate"
-        }, 8000);
-    }
-    textLoad();
-    setInterval(textLoad, 12000);
 
     return (
         <React.Fragment>
@@ -25,91 +9,31 @@ export default function Home() {
 
                 </div>
                 <div className="col-9">
-                    <section className="bg-banner m-2 me-3 text-light p-5">
-                        <h1>
+                    <section className="bg-banner m-2 me-3 text-light p-5 ">
+                        <div className="fs-3 fw-bold ">
                             Hi, I'm E-J
-                        </h1>
-                        <div className="container container-animation">
-                            <span className="text first-text">I'm a </span>
-                            <span className="text sec-text" id="sec-text">Web Developer</span>
+                        </div>
+                        <div className="outer-headings" >
+                            <h1>
+                                <div className="inner-headings">
+                                    <span>
+                                        Full-Stack Developer <br />
+                                        Front-End Developer <br />
+                                        Back-End Developer <br />
+                                        Freelancer <br />
+                                    </span>
+                                </div>
+                            </h1>
                         </div>
                         <div>
-                            An enthusiastic Web Developer with passion for programming and love for work.   
+                            An enthusiastic Web Developer with passion for programming and love for work.
                         </div>
                     </section>
-                    {/* <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div>
-                    <div>This is home</div> */}
+                    
                 </div>
             </div>
         </React.Fragment>
     )
+
+
 }
