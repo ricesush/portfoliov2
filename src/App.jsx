353773fb1d4dc5f1758a3d5ffeac822646b3942b
ricesush <pixelartme@gmail.com>
2023-02-26 23:01:52 +0800

    Test: adjusted z-index of main for links & hover

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <React.Fragment>
       <Preloader/>
-      <div className="container p-0 me-auto d-flex main shadow-custom2 p-0 ">
+      <div className="container p-0 me-auto d-flex main shadow-custom2 p-0">
       
         <Sidebar />
         <section className="col-md-12 overflow my-3">
