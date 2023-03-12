39f054087987ead2e1ce7cbd7f9d79c140c13d5a
ricesush <pixelartme@gmail.com>
2023-03-12 12:28:10 +0800

    + contacts and practice pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,15 @@
 import Test from '../components/Test';
 
 const Contact = () => {
-  return <Test />;
+  return (
+    <>
+      <header className='col-lg-12 '>
+        <h2 className='text-center fs-1 py-lg-5 my-3'>
+          <span className='yellowText'>Contact</span> Details
+        </h2>
+      </header>
+    </>
+  );
 };
 
 export default Contact;
--- src/pages/Learnjs.jsx
+++ src/pages/Learnjs.jsx
@@ -1,4 +1,15 @@
 const Learnjs = () => {
-  return <div>learnjs</div>;
+  return (
+    <>
+      <header className='col-lg-12 py-lg-5 my-3'>
+        <h2 className='text-center fs-1 '>
+          <span className='yellowText'>Javascript</span> Notes
+        </h2>
+        <p className='text-center'>
+          <b>Mini tasks, best practices, some tutorials and more..</b>
+        </p>
+      </header>
+    </>
+  );
 };
 export default Learnjs;
