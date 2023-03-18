cd691c7f28f6cd3efa25b1174cdc537912d8cea6
ricesush <pixelartme@gmail.com>
2023-03-18 17:22:54 +0800

    +addTask() and map() on the content area

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -7,15 +7,28 @@ const TodoList = () => {
   const handleChange = (event) => {
     setNewTask(event.target.value);
   };
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+  };
   return (
     <>
       <div className='addTask col-4 d-flex'>
         <input class='form-control col-auto' onChange={handleChange} />
-        <button type='button' className='btn btn-primary col-auto'>
+        <button
+          type='button'
+          className='btn btn-primary col-auto'
+          onClick={addTask}
+        >
           Add Task
         </button>
       </div>
-      <div className='list'>{newTask}</div>
+      <div className='list'>
+        {todoList.map((task) => {
+          return <div>{task}</div>;
+        })}
+      </div>
     </>
   );
 };
