a8148bd2eda09f952f3f4853df121f7bc5d8d47d
ricesush <pixelartme@gmail.com>
2023-03-18 20:02:51 +0800

    + delete task function

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,5 +1,14 @@
 import { useState } from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  solid,
+  regular,
+  brands,
+  icon,
+} from '@fortawesome/fontawesome-svg-core/import.macro';
+import { Link } from 'react-router-dom';
+
 const TodoList = () => {
   const [todoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState('');
@@ -9,15 +18,21 @@ const TodoList = () => {
   };
 
   const addTask = () => {
-    const newTodoList = [...todoList, newTask];
+    setTodoList([...todoList, newTask]);
+  };
+
+  const deleteTask = (task) => {
+    const newTodoList = todoList.filter((taskFilter) => {
+      return taskFilter !== task;
+    });
     setTodoList(newTodoList);
   };
   return (
     <>
       <div className='addTask col-4 d-flex'>
-        <input class='form-control col-auto' onChange={handleChange} />
+        <input className='form-control col-auto' onChange={handleChange} />
         <button
-          type='button'
+          type='submit'
           className='btn btn-primary col-auto'
           onClick={addTask}
         >
@@ -26,7 +41,23 @@ const TodoList = () => {
       </div>
       <div className='list'>
         {todoList.map((task) => {
-          return <div>{task}</div>;
+          return (
+            <div className='d-flex align-items-center'>
+              {task}
+              <button
+                type='button'
+                className='btn bg-transparent border-0 ps-1'
+                onClick={() => deleteTask(task)}
+              >
+                <FontAwesomeIcon
+                  icon={icon({
+                    name: 'trash-can',
+                    style: 'solid',
+                  })}
+                />
+              </button>
+            </div>
+          );
         })}
       </div>
     </>
