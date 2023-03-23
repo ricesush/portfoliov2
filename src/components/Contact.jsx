c1de3406c57f16634fe0dc03096cdd16cfdbada0
ricesush <pixelartme@gmail.com>
2023-03-23 19:43:47 +0800

    created map function for contact details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import {
 import { faEnvelope, faSquarePhone } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
+  const contacts = [
+    { icon: faEnvelope, details: 'Pixelartme@gmail.com' },
+    { icon: faSquarePhone, details: '+63999-551-4413' },
+  ];
   return (
     <section className='col-md-6 rounded-1 border homeSections'>
       <header className='fs-5 fw-bold position-absolute px-3 py-2 rounded-2 rounded-top-0 rounded-start-0 bg-accent shadow'>
@@ -16,14 +20,14 @@ const Contact = () => {
       </header>
       <div className='pt-4 text-sm-start text-center p-5 mt-5'>
         <div className='justify-content-around'>
-          <div>
-            <FontAwesomeIcon icon={faEnvelope} />
-            <small> Pixelartme@gmail.com</small>
-          </div>
-          <div>
-            <FontAwesomeIcon icon={faSquarePhone} />{' '}
-            <small>+63999-551-4413</small>{' '}
-          </div>
+          {contacts.map((contact) => {
+            return (
+              <div>
+                <FontAwesomeIcon icon={contact.icon} />
+                <small> {contact.details}</small>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
