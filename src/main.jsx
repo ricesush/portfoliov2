daf665be22d06df2395809d1b72d4da297820bc5
ricesush <pixelartme@gmail.com>
2023-03-24 00:21:26 +0800

    changed path from /Docs to /docs

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <App /> },
       { path: '/projects', element: <ProjectSection /> },
-      { path: '/Docs', element: <Docs /> },
+      { path: '/docs', element: <Docs /> },
     ],
   },
 ]);
