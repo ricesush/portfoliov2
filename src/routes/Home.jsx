e526fe99d8c4ac2b38a3c18082487e5794ef0832
ricesush <pixelartme@gmail.com>
2023-03-28 00:51:04 +0800

    fixed preloader

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,14 +3,20 @@ import AboutSection from '../components/AboutSection';
 import Contact from '../components/Contact';
 import TechStack from '../components/TechStack';
 import WorkPreference from '../components/WorkPreference';
-
+import { useState } from 'react';
 import { Milestone } from '../components/Milestone';
 import { Cetrification } from '../components/Certificates';
 
 export const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
-    <div className=''>
-      <section className='border-bottom py-5 shadow headerSection'>
+    <div>
+      <section
+        className={`border-bottom py-5 shadow headerSection ${
+          isLoading ? 'h85' : ''
+        }`}
+        onLoad={() => setIsLoading(false)}
+      >
         <HeaderSectionTest />
       </section>
       <section className='d-lg-flex pt-5 justify-content-around container'>
