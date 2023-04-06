bed545daa196224bc1c569a3276da26ee4a05e51
ricesush <pixelartme@gmail.com>
2023-04-06 14:16:29 +0800

    added content for dependencies

--- src/components/MERNCrud.jsx
+++ src/components/MERNCrud.jsx
@@ -1,3 +1,23 @@
 export function MernCrud() {
-  return <section>This is the MERN Crud section</section>;
+  const dependecies = [
+    { tech: 'express', id: 1 },
+    { tech: 'mongoose', id: 2 },
+    { tech: 'dotenv', id: 3 },
+    { tech: 'nodemon', id: 4 },
+    { tech: 'jsontoken', id: 5 },
+    { tech: 'bcrypt', id: 6 },
+  ];
+
+  return (
+    <section className='px-4'>
+      <h5>Starting the Serverside</h5>
+      <div>Server Side Dependecies:</div>
+      <ul>
+        {dependecies.map((dependency) => {
+          return <li key={dependency.id}>{dependency.tech}</li>;
+        })}
+      </ul>
+      <p></p>
+    </section>
+  );
 }
